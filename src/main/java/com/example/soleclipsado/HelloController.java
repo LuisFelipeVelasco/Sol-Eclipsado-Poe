f961d3453649ccbc80fe82889e95194704f406df
Lol G <jeronimorojasimbachi191@gmail.com>
2026-03-15 12:45:13 -0500

    Methodes changed to boolean If now can detect special characters

--- src/main/java/com/example/soleclipsado/HelloController.java
+++ src/main/java/com/example/soleclipsado/HelloController.java
@@ -18,44 +18,78 @@ public class HelloController {
     String Normalizar; //esta variable sera utilizada para guardar los cambios que se haran para pasar de mayusculas y tildes a solo minusculas
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected boolean onHelloButtonClick() {
+        //GuardarPalabra guarda la palabra escrita pór el usuario
+        GuardarPalabra = palabraField.getText();
+        //GuardarLongitud guarda la longitud de la palabra escrita pór el usuario
         GuardarLongitud= palabraField.getLength();
+        //este if revisa si la palabra guardada tiene entre 6 y 12 letras, caso contrario tira una advertencia y reinicia la palabra a blanco
         if(GuardarLongitud<6 || GuardarLongitud>12){
             welcomeText.setText("La palabra debe ser entre 6 y 12 letras");
             palabraField.setText("");
-            return;
+            return false;
+
+
+            //este if revisa si la palabra tiene espacios o caracteres especiales, caso contrario tira una advertencia y reinicia la palabra a blanco
+        }if (!GuardarPalabra.matches("\\p{L}+")){
+            welcomeText.setText("La palabra no puede tener espacios ni caracteres especiales");
+            palabraField.setText("");
+            return false;
+
+        } else{
+            welcomeText.setText("Welcome to JavaFX Application!");
 
 
 
-        }else {
 
-        }
-
-        welcomeText.setText("Welcome to JavaFX Application!");
-
-
-        //GuardarPalabra guarda la palabra escrita pór el usuario
-        GuardarPalabra = palabraField.getText();
 
 
             Normalizar=Normalizer.normalize(GuardarPalabra,Normalizer.Form.NFD);// Se llama a la libreria Normalizer para usar su funcion normalize y separar las tildes de las letras
-           Normalizar= Normalizar.replaceAll("\\p{M}", "").toLowerCase(Locale.ROOT); // esta funcion utiliza el replaceAll para quitar las tildes y usa la funcion LowerCase para pasar todo a minusculas y guardarlo todo en la variable Normalizer
-        System.out.println(Normalizar);
+            Normalizar= Normalizar.replaceAll("\\p{M}", "").toLowerCase(Locale.ROOT); // esta funcion utiliza el replaceAll para quitar las tildes y usa la funcion LowerCase para pasar todo a minusculas y guardarlo todo en la variable Normalizer
+
+            System.out.println(Normalizar);
+            return true;
+        }
+
+
 
     }
     @FXML
     private TextField palabraField;
 
     @FXML
-    protected void onWordRedaction(){
-        welcomeText.setText("Welcome to JavaFX Application!");
-
-
+    protected boolean onWordRedaction(){
+        //GuardarPalabra guarda la palabra escrita pór el usuario
         GuardarPalabra = palabraField.getText();
+        //GuardarLongitud guarda la longitud de la palabra escrita pór el usuario
+        GuardarLongitud= palabraField.getLength();
+        //este if revisa si la palabra guardada tiene entre 6 y 12 letras, caso contrario tira una advertencia y reinicia la palabra a blanco
+        if(GuardarLongitud<6 || GuardarLongitud>12){
+            welcomeText.setText("La palabra debe ser entre 6 y 12 letras");
+            palabraField.setText("");
+            return false;
 
 
-        Normalizar=Normalizer.normalize(GuardarPalabra,Normalizer.Form.NFD);
-        Normalizar= Normalizar.replaceAll("\\p{M}", "").toLowerCase(Locale.ROOT);
+            //este if revisa si la palabra tiene espacios o caracteres especiales, caso contrario tira una advertencia y reinicia la palabra a blanco
+        }if (!GuardarPalabra.matches("\\p{L}+")){
+            welcomeText.setText("La palabra no puede tener espacios ni caracteres especiales");
+            palabraField.setText("");
+            return false;
+
+        } else{
+            welcomeText.setText("Welcome to JavaFX Application!");
+
+
+
+
+
+
+            Normalizar=Normalizer.normalize(GuardarPalabra,Normalizer.Form.NFD);// Se llama a la libreria Normalizer para usar su funcion normalize y separar las tildes de las letras
+            Normalizar= Normalizar.replaceAll("\\p{M}", "").toLowerCase(Locale.ROOT); // esta funcion utiliza el replaceAll para quitar las tildes y usa la funcion LowerCase para pasar todo a minusculas y guardarlo todo en la variable Normalizer
+
+            System.out.println(Normalizar);
+            return true;
+        }
 
 
 
