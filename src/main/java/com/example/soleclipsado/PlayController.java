db0aea9d4068e2cb86f365055e5effda9d171489
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-03-16 10:19:12 -0500

    Create AsignarSetOnKeyTypedACamposDeTexto()

--- src/main/java/com/example/soleclipsado/PlayController.java
+++ src/main/java/com/example/soleclipsado/PlayController.java
@@ -1,6 +1,7 @@
 package com.example.soleclipsado;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 
 import java.net.PasswordAuthentication;
@@ -25,31 +26,43 @@ public class PlayController {
     //Metodo que va a orquestar la interfaz de adivinar la palabra
     public void AdivinarPalabra(String Palabra) {
         PalabraSecreta=Palabra;
-        CrearCamposdeTexto();
+        CrearCamposdeTextoEnInterfaz();
+        AsignarSetOnKeyTypedACamposDeTexto();
     }
 
     //Metodo para crear los campos de texto de acuerdo a la longitud de la palabra
-    protected void CrearCamposdeTexto(){
+    protected void CrearCamposdeTextoEnInterfaz(){
 
         Integer GuardarLongitudPalabraSecreta= PalabraSecreta.length();
         for(Integer i=0; i< GuardarLongitudPalabraSecreta; i++){
-            TextField textField = getTextField();
-            textFields.add(textField); //Añade al objeto textField en la lista de textFields
-            hbox.getChildren().add(textField); //Agrega el textField en el hbox
+            TextField textField = GetTextField(); //Instancia un campo de texto con estilo
+            textFields.add(textField); //Añade al objeto texfield en la lista de textFields
+            hbox.getChildren().add(textField); //Agrega el campo de texto en el hbox
         }
 
     }
     //Retorna un textField con un estilo
-    protected TextField getTextField() {
+    protected TextField GetTextField() {
         TextField textField= new TextField(); //Instancia un nuevo texfield
         textField.setStyle( "-fx-font-size: 20; " + "-fx-font-family: 'Arial'; " + "-fx-font-weight: bold; " + //Estilo para textfield
-                        "-fx-alignment: center; "  + "-fx-border-color: #34495e; " + "-fx-border-width: 1; " + "-fx-border-radius: 2;" + "-fx-pref-height: 20;"+"-fx-min-height: 40; " +
+                        "-fx-alignment: center; "  + "-fx-border-color: #34495e; " + "-fx-border-width: 1; " + "-fx-border-radius: 2;" +"-fx-min-height: 40; " +
                 "-fx-max-height: 40; " +
                 "-fx-pref-height: 40; "
         );
         return textField;
     }
 
+    //Le asigna a cada texfield el event listener setOnKeyTyped y hace que VerificarPalabra se encargue del evento
+    protected void AsignarSetOnKeyTypedACamposDeTexto(){
+        for (TextField textField : textFields){
+            textField.setOnKeyTyped(this::VerificarPalabra);
+        }
+    }
+    protected void VerificarPalabra(KeyEvent keyEvent){
+
+    }
 
 }
+
+
 
